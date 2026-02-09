16836c5a055415c9b45dfc95f252d54950048e0f
perumallaharshitha <harshithaperumalla23@gmail.com>
2026-02-09 20:24:19 +0530

    updated with frontend url

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: process.env.CLIENT_URL || "https://ledgerly-ivory.vercel.app",
     credentials: true,
   })
 );
@@ -26,6 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/wallet", walletRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.get("/", (req, res) => res.send("Ledgerly API is running!"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
