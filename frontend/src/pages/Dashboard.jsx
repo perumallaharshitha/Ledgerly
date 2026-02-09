a8f309b9c5c8b28cbd57be7fb6ccd2c1d4699f26
perumallaharshitha <harshithaperumalla23@gmail.com>
2026-02-09 20:09:15 +0530

    final commit

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Card from "../components/Card";
-import "./Dashboard.css";
+import "./DashBoard.css";
 
 function Dashboard({ onLogout, user }) {
   const navigate = useNavigate();
