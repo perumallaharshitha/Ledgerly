93f983899a43e079319c2599031718fa180e2de1
perumallaharshitha <harshithaperumalla23@gmail.com>
2026-02-09 18:55:34 +0530

    updated vercel.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -15,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*", 
+    origin: process.env.CLIENT_URL || "*",
     credentials: true,
   })
 );
@@ -23,7 +25,8 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/wallet", walletRoutes);
 app.use("/api/transactions", transactionRoutes);
-
 app.get("/", (req, res) => res.send("Ledgerly API is running!"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
